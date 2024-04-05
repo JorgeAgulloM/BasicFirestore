86fef8408a319f9d9dfaf75ca106f10166b48096
Jorge1 <agullojorge@gmail.com>
2024-04-05 07:59:50 +0200

    Basic insert

--- app/src/main/java/com/softyorch/basicfirestore/MainActivity.kt
+++ app/src/main/java/com/softyorch/basicfirestore/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.softyorch.basicfirestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firestore: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firestore = Firebase.firestore
+        basicInsert()
+    }
+
+    private fun basicInsert() {
+        val user = hashMapOf(
+            "name" to "Yorch",
+            "age" to 30,
+            "happy" to true,
+            "extraInfo" to null
+        )
+
+        //firestore.collection("users").add(user)
+        //val isSuccessful = firestore.collection("users").add(user).isSuccessful
+        //firestore.collection("users").add(user).await()
+        firestore.collection("users").add(user)
+            .addOnSuccessListener {
+                Log.i("LOGTA", "Success")
+            }.addOnFailureListener {
+                Log.e("LOGTAG", "Error: ${it.message}")
+            }
     }
 }
